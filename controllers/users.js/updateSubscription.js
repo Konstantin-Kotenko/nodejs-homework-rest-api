dd3e7a1e9e2eb7f41cccedb705d6cb490e02d91a
Konstantin-Kotenko <kostya.kotenkoo@gmail.com>
2022-11-08 12:02:40 +0200

    fix updateSubscription

--- controllers/users.js/updateSubscription.js
+++ controllers/users.js/updateSubscription.js
@@ -3,7 +3,7 @@ const {User} = require('../../models/user');
 const updateSubscription = async (req, res) => {
   const {_id} = req.user;
   const {email, subscription} = req.body;
-  await User.findByIdAndUpdate(_id, {email, subscription}, {new: true});
+  await User.findByIdAndUpdate(_id, {subscription}, {new: true});
   res.status(200).json({
     email,
     subscription,
